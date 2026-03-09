f96b61cca9ee652b08279657ed012721551094bb
Danikul44444 <acriliant@gmail.com>
2026-03-09 10:13:36 +0300

    Data collection

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ from parse.book24 import book24_parse
 from parse.labirint import labirint_parse
 from parse.bookvoed import bookvoed_parse
 from threading import Thread
->>>>>>> c8e6725 (Added three parser)
 import sqlite3
 import time
 import os 
-CREATE_TABLE = """CREATE TABLE BOOK(
+CREATE_TABLE = """CREATE TABLE IF NOT EXISTS BOOK(
     TITLE TEXT,
     PRICE INTEGER,
     LINK TEXT,
@@ -26,8 +25,3 @@ time.sleep(3)
 labirint_parse(search, "BOOK")
 time.sleep(3)
 bookvoed_parse(search, "BOOK")
-<<<<<<< HEAD
-bookvoed_parse(search, "BOOK")
-=======
-
->>>>>>> 76b1e63 (Add README.md)
